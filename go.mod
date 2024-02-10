c23c7a084d88f3580c27f77135b4796bb190dbe4
jeff H <jeffrey92@gmail.com>
2024-02-09 21:53:28 -0700

    panic: add stack info to unhandled panics
    copied from: https://github.com/RealHarshThakur/work/tree/stacktrace
    
    log with logrus so panics make it into logs

--- go.mod
+++ go.mod
@@ -21,8 +21,11 @@ require (
 	github.com/orfjackal/nanospec.go v0.0.0-20120727230329-de4694c1d701 // indirect
 	github.com/robfig/cron v1.2.0 // indirect
 	github.com/robfig/cron/v3 v3.0.1
+	github.com/sirupsen/logrus v1.7.0
 	github.com/stretchr/testify v1.7.0
 	github.com/youtube/vitess v2.1.1+incompatible // indirect
 	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
 	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
+
+replace github.com/sirupsen/logrus => github.com/someburner/logrus v1.8.1
--- log.go
+++ log.go
@@ -1,7 +1,19 @@
 package work
 
-import "fmt"
+import (
+	"encoding/json"
+	log "github.com/sirupsen/logrus"
+)
 
 func logError(key string, err error) {
-	fmt.Printf("ERROR: %s - %s\n", key, err.Error())
+	log.Errorf("work: %s - %s\n", key, err.Error())
+}
+
+func logPanic(panic Error) {
+	stack, err := json.Marshal(panic.Stack)
+	if err != nil {
+		log.Errorf("runJob.panic - %s\n", panic.Error())
+		log.Errorf("Marshalling panic: runJob.panic - %s\n", err.Error())
+	}
+	log.Errorf("runJob.panic: %s\n %s - %s \n %s - %s \n", string(stack), "error message", panic.Error(), "error class", panic.Class)
 }
